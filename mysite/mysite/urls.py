881d6b8c3929ec26166d55042e0eb501617d69d0
nakaji-nash <nakaji.m.0625@gmail.com>
2020-12-19 22:46:32 +0900

    edited URL

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+#include()関数は他のURLconfへ参照することができる。
 urlpatterns = [
+    
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
